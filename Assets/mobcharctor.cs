b8a6eb100aa8d01dae4d599006857ba3ded08e49
WIN8THS40779\Ando Hiroki <ashsavior17@gmail.com>
2017-11-02 12:44:20 +0900

    結合用の最低限の機能のプロジェクト

--- Assets/mobcharctor.cs
+++ Assets/mobcharctor.cs
@@ -19,12 +19,8 @@ public class mobcharctor : MonoBehaviour
     //グローバル変数
     //=========================================
     public Mode m_Mode;
-    private Animator animator;
     private bool Active;//updateの有効化on/off
     public Transform player;    //プレイヤーを代入
-    public Transform goal0;
-    public Transform goal1;
-    public Transform goal2;
     public float speed = 1; //移動速度
     public float limitDistance = 0; //敵キャラクターがどの程度近づいてくるか設定(この値以下には近づかない）
 
@@ -38,19 +34,11 @@ public class mobcharctor : MonoBehaviour
     //=========================================
     void Start()
     {
-
         m_Mode = Mode.UROURO;
         Active = true;
-        animator = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        goal0  = GameObject.FindGameObjectWithTag("Goal0").transform;
-        goal1  = GameObject.FindGameObjectWithTag("Goal1").transform;
-        goal2  = GameObject.FindGameObjectWithTag("Goal2").transform;
 
         limitDistance = 10f;
-
-
-
 
     }
     //=========================================
@@ -64,26 +52,6 @@ public class mobcharctor : MonoBehaviour
         {
             case Mode.UROURO:
 
-                if (changetime < Time.time)
-                {
-                    ////更新時刻が来た
-                    //animator.SetBool("isrunning", false);
-                    //animator.SetBool("iswalking", true);
-                    //int goal = UnityEngine.Random.Range(0, 2);    //ランダムで目的地を変える
-                    //GameObject goalobj = GameObject.Find("Goal" + goal);
-                    //GameObject obj = GameObject.Find("Mob1");
-                    //NavMeshAgent nma = obj.GetComponent<NavMeshAgent>();
-                    //nma.SetDestination(goalobj.transform.position);
-                    //changetime = Time.time + UnityEngine.Random.Range(5f, 10f);  //次の更新時刻を決める
-                    ////Debug.Log("歩行中");
-                }
-
-                else
-                {
-                    animator.SetBool("isrunning", false);
-                    animator.SetBool("iswalking", false);
-                }
-
                 break;
             case Mode.FOLLOW:
 
@@ -95,16 +63,9 @@ public class mobcharctor : MonoBehaviour
                 //プレイヤーの距離が一定以上でなければ、敵キャラクターはプレイヤーへ近寄ろうとしない
                 if (distance >= limitDistance)
                 {
-                    animator.SetBool("iswalking", false);
-                    animator.SetBool("isrunning", true);
                     //プレイヤーとの距離が制限値以上なので普通に近づく
                     transform.position = transform.position + (direction * speed * Time.deltaTime);
 
-                }
-                else if (distance < limitDistance)
-                {
-                    animator.SetBool("iswalking", false);
-                    animator.SetBool("isrunning", false);
                 }
                 //プレイヤーの方を向く
                 transform.rotation = Quaternion.LookRotation(direction);
@@ -113,24 +74,9 @@ public class mobcharctor : MonoBehaviour
             case Mode.EVENT:
 
                 break;
-            
         }
-        Debug.Log(m_Mode);
-        //=======================================
-        //モブの状態操作
-        //=======================================
-        if (Input.GetKeyDown("1"))
-        {
-            m_Mode = Mode.UROURO;
-        }
-        else if (Input.GetKeyDown("2"))
-        {
-            m_Mode = Mode.FOLLOW;
-        }
-        else if (Input.GetKeyDown("3"))
-        {
-            m_Mode = Mode.WAIT;
-        }
+ 
+
 
     }
     //=========================================
@@ -141,6 +87,5 @@ public class mobcharctor : MonoBehaviour
     public void SetMode(Mode mode)
     {
         m_Mode = mode;
-
     }
 }
